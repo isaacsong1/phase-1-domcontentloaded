24a1ead0a703eb216f4292e4a6ae40c283e28451
Isaac Song <isaacsongs10@gmail.com>
2023-09-13 14:05:32 -0700

    DOMContentLoaded Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    console.log('After DOM is loaded');
+    let newP = document.querySelector("#text");
+    newP.textContent = 'This is really cool!';
+    console.log(newP.textContent);
+});
+
+console.log('Before DOM is loaded');
+console.log(document.querySelector('#text').textContent);
